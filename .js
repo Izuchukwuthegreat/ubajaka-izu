c7c2d79fd660b9dfbaf6fdb7b251ff54ea301982
Izuchukwuthegreat <161133073+Izuchukwuthegreat@users.noreply.github.com>
2024-09-25 23:46:06 +0100

    Add files via upload

---
+++ .js
@@ -0,0 +1,13 @@
+// Traditional function
+function add(a, b) {
+    return a + b;
+}
+
+// Arrow function
+const add = (a, b) => a + b;
+
+const areaOfRectangle = (length, width) => length * width;
+console.log(areaOfRectangle(5, 10)); // Output: 50
+
+const cube = (n) => n ** 3;
+console.log(cube(5)); // Output: 125
